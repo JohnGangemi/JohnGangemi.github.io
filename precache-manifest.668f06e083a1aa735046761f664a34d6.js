7e7e9c24c3a54b3ab23389c8f775db0592c74877
John Anthony Gangemi <JohnAGangemi@gmail.com>
2018-12-04 19:26:54 -0500

    Updates

---
+++ precache-manifest.668f06e083a1aa735046761f664a34d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e97916ec5a7ff36cd37e2c68b2d0ff10",
+    "url": "/static/media/avatar.e97916ec.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7aca3edce2ef27d1ace1",
+    "url": "/static/js/main.7aca3edc.chunk.js"
+  },
+  {
+    "revision": "35f1efb2ce850c100643",
+    "url": "/static/js/1.35f1efb2.chunk.js"
+  },
+  {
+    "revision": "7aca3edce2ef27d1ace1",
+    "url": "/static/css/main.0bc43678.chunk.css"
+  },
+  {
+    "revision": "bfc78074ea0c537179f1a08dea26f1cc",
+    "url": "/index.html"
+  }
+];
