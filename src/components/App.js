001c4b562b955ff37d864f9ee46af415802d65cc
John Anthony Gangemi <JohnAGangemi@gmail.com>
2019-01-09 14:39:19 -0500

    add color wheel

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,23 @@ import Resume from '../components/Resume.js';
 import Projects from '../components/Projects.js';
 import Contact from '../components/Contact.js';
 import Menu from '../components/Menu.js';
+import Wheel from '../components/Wheel.js';
 import '../styles/App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isMenuOpen: false
+      isMenuOpen: false,
+      backgroundColor: 'none'
     };
 
     this.menuToogle = this.menuToogle.bind(this);
     this.menuClose = this.menuClose.bind(this);
+    this.setBackgroundBlue = this.setBackgroundBlue.bind(this);
+    this.setBackgroundGreen = this.setBackgroundGreen.bind(this);
+    this.setBackgroundOrange = this.setBackgroundOrange.bind(this);
+    this.setBackgroundRed = this.setBackgroundRed.bind(this);
   }
 
   menuToogle() {
@@ -30,9 +36,52 @@ class App extends Component {
     this.setState({isMenuOpen: false});
   }
 
+  setBackgroundGreen() {
+    this.setState({backgroundColor: 'green'});
+  }
+
+  setBackgroundRed() {
+    this.setState({backgroundColor: 'red'});
+  }
+
+  setBackgroundOrange() {
+    this.setState({backgroundColor: 'orange'});
+  }
+
+  setBackgroundBlue() {
+    this.setState({backgroundColor: 'blue'});
+  }
+
   render() {
+    const appBackgroundColor = this.state.backgroundColor;
+    var backgroundStyle;
+    switch (appBackgroundColor) {
+      case 'green':
+        backgroundStyle = {
+          backgroundColor: 'rgb(204, 255, 204)'
+        }
+        break;
+      case 'red':
+        backgroundStyle = {
+          backgroundColor: 'rgb(255, 204, 204)'
+        }
+        break;
+      case 'orange':
+        backgroundStyle = {
+          backgroundColor: 'rgb(255, 235, 204)'
+        }
+        break;
+      case 'blue':
+        backgroundStyle = {
+          backgroundColor: 'rgb(204, 204, 255)'
+        }
+        break;
+      default: backgroundStyle = {}
+    }
+
     return (
-      <div className="App">
+      <div className="App" style={backgroundStyle}>
+        <Wheel position={{upperLeft: this.setBackgroundGreen, upperRight: this.setBackgroundRed, lowerRight: this.setBackgroundOrange, lowerLeft: this.setBackgroundBlue}} />
         <Nav burger={this.menuToogle} />
         <Menu action={{close: this.menuClose, open: this.state.isMenuOpen}} />
         <Switch>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -34,41 +34,41 @@ class Resume extends Component {
 
         return(
             <div className="Resume" id="Resume" ref={this.ref}>
-            <div className="Resume-detail">
-                <p className="Resume-title">SRI International, 2016 - Present</p>
-                <ProgressBar fill={contentDivisionWidth}/>
-                <p className="Resume-description">
-                    Software Engineer in Advanced Technology and Science Division of SRI International. 
-                    Supporting all aspects of web services in an agile environment utilizing the latest 
-                    web technologies and advanced analytics platforms. Maintaining legacy systems and 
-                    radar/sensor connectors. Assisted in architecture design of various software frameworks 
-                    related to company intellectual property.
-                    <br/><br/>
-                    Participated in Internal Research and Development programs, Off-site technical demonstrations 
-                    and client support, proposal acquisitions and proposal submissions, and white paper development.
-                </p>
-            </div>
-            <div className="Resume-spacer-vertical"/>
-            <div className="Resume-detail">
-                <p className="Resume-title">Bankers Financial Corporation, 2015</p>
-                <ProgressBar fill={contentDivisionWidth * 2}/>
-                <p className="Resume-description">
-                    Developed a version control utility to track individual contributor stats for Bankers IT 
-                    department using C# and Windows Forms framework. Assisted in the redesign and development of 
-                    a corporate web application using ASP.NET and other web technologies.
-                </p>
-            </div>
-            <div className="Resume-spacer-vertical"/>
-            <div className="Resume-detail">
-                <p className="Resume-title">Undergraduate Research, 2015</p>
-                <ProgressBar fill={currentContentWidth}/>
-                <p className="Resume-description">
-                    Researched the design and synthesis of combinational feedback logic with FPGAs. In particular, 
-                    I attempted to mitigate the complications that arise when modeling asynchronous combinational 
-                    circuits in FPGA fabric and sought-out solutions from published scholarly journals, web articles, 
-                    online forums, and professionals in the industry.
-                </p>
-            </div>
+                <div className="Resume-detail">
+                    <p className="Resume-title">SRI International, 2016 - Present</p>
+                    <ProgressBar fill={contentDivisionWidth}/>
+                    <p className="Resume-description">
+                        Software Engineer in Advanced Technology and Science Division of SRI International. 
+                        Supporting all aspects of web services in an agile environment utilizing the latest 
+                        web technologies and advanced analytics platforms. Maintaining legacy systems and 
+                        radar/sensor connectors. Assisted in architecture design of various software frameworks 
+                        related to company intellectual property.
+                        <br/><br/>
+                        Participated in Internal Research and Development programs, Off-site technical demonstrations 
+                        and client support, proposal acquisitions and proposal submissions, and white paper development.
+                    </p>
+                </div>
+                <div className="Resume-spacer-vertical"/>
+                <div className="Resume-detail">
+                    <p className="Resume-title">Bankers Financial Corporation, 2015</p>
+                    <ProgressBar fill={contentDivisionWidth * 2}/>
+                    <p className="Resume-description">
+                        Developed a version control utility to track individual contributor stats for Bankers IT 
+                        department using C# and Windows Forms framework. Assisted in the redesign and development of 
+                        a corporate web application using ASP.NET and other web technologies.
+                    </p>
+                </div>
+                <div className="Resume-spacer-vertical"/>
+                <div className="Resume-detail">
+                    <p className="Resume-title">Undergraduate Research, 2015</p>
+                    <ProgressBar fill={currentContentWidth}/>
+                    <p className="Resume-description">
+                        Researched the design and synthesis of combinational feedback logic with FPGAs. In particular, 
+                        I attempted to mitigate the complications that arise when modeling asynchronous combinational 
+                        circuits in FPGA fabric and sought-out solutions from published scholarly journals, web articles, 
+                        online forums, and professionals in the industry.
+                    </p>
+                </div>
             </div>
         );
     }
